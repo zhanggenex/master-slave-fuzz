d87f6604e79f1e452af25aa50e3f94fdd847619e
zhanggenex <zhanggenex@qq.com>
2019-06-28 04:43:08 -0700

    m

---
+++ run.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+time="$1"
+
+log-debug() {
+    echo -e "\e[94m[*]\e[0m $*"
+}
+
+print-args()
+{
+    [[ "$#" -le 2 ]] && return
+    log-debug "$1"
+    for arg in "${@:2}"; do
+        log-debug "\t$arg"
+    done
+}
+
+args=("../afl-fuzz" "-m 20981520" "-t 1000" "-i" "seed" "-o" "out" "-M" "master" "./aflapp")
+print-args "AFL cmdline:" "${args[@]}"
+nohup timeout $time "${args[@]}" > master.log  &
+
+args=("../afl-fuzz" "-m 20981520" "-t 1000" "-i" "seed" "-o" "out" "-S" "fuzzer1" "./aflapp")
+print-args "AFL cmdline:" "${args[@]}"
+nohup timeout $time "${args[@]}" > fuzzer1.log  &
+
+args=("../afl-fuzz" "-m 20981520" "-t 1000" "-i" "seed" "-o" "out" "-S" "fuzzer2" "./aflapp")
+print-args "AFL cmdline:" "${args[@]}"
+nohup timeout $time "${args[@]}" > fuzzer2.log  &
+
+args=("../afl-fuzz" "-m 20981520" "-t 1000" "-i" "seed" "-o" "out" "-S" "fuzzer3" "./aflapp")
+print-args "AFL cmdline:" "${args[@]}"
+nohup timeout $time "${args[@]}" > fuzzer3.log  &
+
+args=("../afl-fuzz" "-m 20981520" "-t 1000" "-i" "seed" "-o" "out" "-S" "fuzzer4" "./aflapp")
+print-args "AFL cmdline:" "${args[@]}"
+nohup timeout $time "${args[@]}" > fuzzer4.log
+
